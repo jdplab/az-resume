83fd3f9156cae2660a73af4dd61b897d671389b2
jplab <jon@jon-polansky.com>
2024-03-03 16:12:50 -0500

    Changed to primary_web_host

--- main.tf
+++ main.tf
@@ -51,9 +51,9 @@ resource "azurerm_cdn_endpoint" "resume" {
   profile_name        = azurerm_cdn_profile.resume.name
   resource_group_name = azurerm_resource_group.resume.name
   location            = azurerm_resource_group.resume.location
-  origin_host_header  = azurerm_storage_account.resume.primary_web_endpoint
+  origin_host_header  = azurerm_storage_account.resume.primary_web_host
     origin {
       name            = "resume-origin"
-      host_name       = azurerm_storage_account.resume.primary_web_endpoint
+      host_name       = azurerm_storage_account.resume.primary_web_host
     }
 }
