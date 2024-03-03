d1a92957ceed4c1594df7dc28a2c0cf2267c29a7
jplab <jon@jon-polansky.com>
2024-03-03 16:01:35 -0500

    Added cdn frontdoor profile and endpoint

--- main.tf
+++ main.tf
@@ -38,3 +38,21 @@ resource "azurerm_storage_account" "resume" {
     index_document = "index.html"
   }
 }
+
+resource "azurerm_cdn_frontdoor_profile" "resume" {
+  name                = "resume-cdn"
+  resource_group_name = azurerm_resource_group.resume.name
+  sku_name            = "Standard_AzureFrontDoor"
+}
+
+resource "azurerm_cdn_endpoint" "resume" {
+  name                = "resume-cdn-endpoint"
+  profile_name        = azurerm_cdn_frontdoor_profile.resume.name
+  resource_group_name = azurerm_resource_group.resume.name
+  location            = azurerm_resource_group.resume.location
+  origin_host_header  = azurerm_storage_account.resume.primary_web_endpoint
+    origin {
+      name            = "resume-origin"
+      host_name       = azurerm_storage_account.resume.primary_web_endpoint
+    }
+}
