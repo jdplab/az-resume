99141fda4fb66a543b486b089ac719d5b453f9db
jplab <jon@jon-polansky.com>
2024-03-03 16:41:25 -0500

    Added custom domain and cloudflare configuration

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
 
   backend "remote" {
-    organization = "JP-Lab"
+    organization           = "JP-Lab"
 
     workspaces {
-      name = "azure-resume"
+      name                 = "azure-resume"
     }
   }
 }
@@ -12,8 +12,13 @@ terraform {
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.94.0"
+      source               = "hashicorp/azurerm"
+      version              = "3.94.0"
+    }
+
+    cloudflare = {
+      source               = "cloudflare/cloudflare"
+      version              = "4.25.0"
     }
   }
 }
@@ -22,9 +27,13 @@ provider "azurerm" {
   features {}
 }
 
+provider "cloudflare" {
+  api_token                = VAR.CLOUDFLARE_API_TOKEN
+}
+
 resource "azurerm_resource_group" "resume" {
-  name     = var.RESOURCE_GROUP_NAME
-  location = var.LOCATION
+  name                     = var.RESOURCE_GROUP_NAME
+  location                 = var.LOCATION
 }
 
 resource "azurerm_storage_account" "resume" {
@@ -35,25 +44,43 @@ resource "azurerm_storage_account" "resume" {
   account_replication_type = "LRS"
 
   static_website {
-    index_document = "index.html"
+    index_document         = "index.html"
   }
 }
 
 resource "azurerm_cdn_profile" "resume" {
-  name                = "resume-cdn-profile"
-  resource_group_name = azurerm_resource_group.resume.name
-  location            = azurerm_resource_group.resume.location
-  sku                 = "Standard_Microsoft"
+  name                     = "resume-cdn-profile"
+  resource_group_name      = azurerm_resource_group.resume.name
+  location                 = azurerm_resource_group.resume.location
+  sku                      = "Standard_Microsoft"
 }
 
 resource "azurerm_cdn_endpoint" "resume" {
-  name                = "resume-cdn-endpoint"
-  profile_name        = azurerm_cdn_profile.resume.name
-  resource_group_name = azurerm_resource_group.resume.name
-  location            = azurerm_resource_group.resume.location
-  origin_host_header  = azurerm_storage_account.resume.primary_web_host
+  name                     = "resume-cdn-endpoint"
+  profile_name             = azurerm_cdn_profile.resume.name
+  resource_group_name      = azurerm_resource_group.resume.name
+  location                 = azurerm_resource_group.resume.location
+  origin_host_header       = azurerm_storage_account.resume.primary_web_host
     origin {
-      name            = "resume-origin"
-      host_name       = azurerm_storage_account.resume.primary_web_host
+      name                 = "resume-origin"
+      host_name            = azurerm_storage_account.resume.primary_web_host
+    }
+}
+
+resource "cloudflare_record" "resume" {
+  zone_id                  = var.CLOUDFLARE_ZONE_ID
+  name                     = var.DOMAIN_NAME
+  value                    = azurerm_cdn_endpoint.resume.host_name
+  type                     = "CNAME"
+  proxied                  = true
+}
+
+resource "azurerm_cdn_endpoint_custom_domain" "resume" {
+  name                     = "resume-cdn-endpoint-custom-domain"
+  cdn_endpoint_id          = azurerm_cdn_endpoint.resume.id
+  host_name                = var.DOMAIN_NAME
+    cdn_managed_https {
+      certificate_type     = "dedicated"
+      protocol_type        = "ServerNameIndication"
     }
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,27 @@
 variable "RESOURCE_GROUP_NAME" {
-    type = string
-    default = "Az-Resume"
+  type = string
+  default = "Az-Resume"
 }
 
 variable "LOCATION" {
-    type = string
-    default = "East US"
+  type = string
+  default = "East US"
 }
 
 variable "STORAGE_ACCOUNT_NAME" {
   type = string
   default = "jpolanskyresume"
+}
+
+variable "DOMAIN_NAME" {
+  type = string
+  default = "azureresume.jon-polansky.com"
+}
+
+variable "CLOUDFLARE_API_TOKEN" {
+  type = string
+}
+
+variable "CLOUDFLARE_ZONE_ID" {
+  type = string
 }
