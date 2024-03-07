d662f1cc9a9f323d7a243bc50d45c594b370cd02
jplab <jon@jon-polansky.com>
2024-03-07 11:55:50 -0500

    Upload new website template

---
+++ web/assets/js/visitorcount.js
@@ -0,0 +1,13 @@
+const functionApi = "https://jpolanskyresume-functionapp.azurewebsites.net/api/app?code=d1xF9BS-fJnjY0zcXgafkMnIFYgXguGZD6MrvVFPR6eEAzFuTY1uxA==";
+    const getVisitCount = () => {
+        return fetch(functionApi)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('unique_visitors').innerText = data.unique_visitors;
+            document.getElementById('total_visits').innerText = data.total_visits;
+            document.getElementById('visits').innerText = data.visits;
+        }).catch(function(error) {
+            console.log(error);
+        });
+    };
+    getVisitCount();
