f6be8db2c2582c31a94a06c33762537e5e0c003b
jplab <jon@jon-polansky.com>
2024-03-19 13:08:27 -0400

    Replaced requests with http.client

--- function/verifytoken/__init__.py
+++ function/verifytoken/__init__.py
@@ -1,27 +1,24 @@
 import logging
-import requests
+import http.client
 from azure.functions import HttpRequest, HttpResponse
-from jose import jwt, jwk, JWTError
+from jose import jwt, JWTError
 import json
 
 def main(req: HttpRequest) -> HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    # Get the id_token from the request headers
     id_token = req.headers.get('Authorization')
 
-    # If the id_token exists
     if id_token:
         try:
-            # Fetch the JWKS from Azure B2C
-            jwks_uri = 'https://azresume.b2clogin.com/azresume.onmicrosoft.com/B2C_1_SignUpSignIn/discovery/v2.0/keys'
-            response = requests.get(jwks_uri)
-            jwks = response.json()
+            jwks_uri = 'azresume.b2clogin.com'
+            conn = http.client.HTTPSConnection(jwks_uri)
+            conn.request("GET", "/azresume.onmicrosoft.com/B2C_1_SignUpSignIn/discovery/v2.0/keys")
+            response = conn.getresponse()
+            jwks = json.loads(response.read().decode())
 
-            # Get the header of the JWT
             headers = jwt.get_unverified_header(id_token)
 
-            # Find the key in the JWKS that matches the kid in the JWT header
             rsa_key = {}
             for key in jwks['keys']:
                 if key['kid'] == headers['kid']:
@@ -33,14 +30,11 @@ def main(req: HttpRequest) -> HttpResponse:
                         'e': key['e']
                     }
 
-            # Verify the id_token and extract the claims
             token_claims = jwt.decode(id_token, rsa_key, algorithms=['RS256'])
 
-            # If the extension_CanEdit claim is "1", return a response indicating that the user has admin rights
             if token_claims.get('extension_CanEdit') == "1":
                 return HttpResponse(json.dumps({"admin": True}), status_code=200, mimetype="application/json")
         except JWTError:
             pass
 
-    # If the id_token does not exist or the extension_CanEdit claim is not "1", return a response indicating that the user does not have admin rights
     return HttpResponse(json.dumps({"admin": False}), status_code=200, mimetype="application/json")
