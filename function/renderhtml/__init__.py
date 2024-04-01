7ff12ad913122202f71f1495dfec2b972794255d
jplab <jon@jon-polansky.com>
2024-04-01 12:49:58 -0400

    WIP - Attempting to implement server-side rendering for SEO

--- function/renderhtml/__init__.py
+++ function/renderhtml/__init__.py
@@ -2,6 +2,7 @@ import azure.functions as func
 from azure.storage.blob import BlobServiceClient, ContentSettings
 from azure.core.exceptions import AzureError
 import os
+import logging
 
 def main(documents: func.DocumentList, context: func.Context) -> str:
     if documents:
@@ -9,30 +10,31 @@ def main(documents: func.DocumentList, context: func.Context) -> str:
             try:
                 post = doc.to_dict()
             except Exception as e:
-                context.log.error(f"Error converting document to dictionary: {str(e)}")
-                continue
+                logging.error(f"Error converting document to dictionary: {str(e)}")
+                return
 
             try:
                 html = render_html_page(post)
             except Exception as e:
-                context.log.error(f"Error rendering HTML page: {str(e)}")
-                continue
+                logging.error(f"Error rendering HTML page: {str(e)}")
+                return
 
             try:
                 blob_service_client = BlobServiceClient.from_connection_string(os.getenv('STORAGE_CONNECTIONSTRING'))
                 blob_client = blob_service_client.get_blob_client(os.getenv('WEB_CONTAINER'), 'posts/' + post['id'] + 'blogpost.html')
             except Exception as e:
-                context.log.error(f"Error getting environment variables or creating Blob client: {str(e)}")
-                continue
+                logging.error(f"Error getting environment variables or creating Blob client: {str(e)}")
+                return
 
             try:
                 cnt_settings = ContentSettings(content_type='text/html')
                 blob_client.upload_blob(html, blob_type="BlockBlob", content_settings=cnt_settings, overwrite=True)
             except AzureError as e:
-                context.log.error(f"Error saving HTML page in Azure Blob Storage: {str(e)}")
-                continue
+                logging.error(f"Error saving HTML page in Azure Blob Storage: {str(e)}")
+                return
 
-        context.log.info("HTML page generated and saved in Azure Blob Storage")
+        logging.info("HTML page generated and saved in Azure Blob Storage")
+        return
 
 def render_html_page(post):
     tags = ",".join(post['tags'])
@@ -47,8 +49,8 @@ def render_html_page(post):
         <meta name="keywords" content="{tags}"/>
         <meta name="description" content="{post['title']}"/>
         <meta name="author" content="Jonathan Polansky"/>
-        <link rel="icon" href="images/terminal.ico">
-		<link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="icon" href="/images/terminal.ico">
+		<link rel="stylesheet" href="/assets/css/main.css" />
 		<script src="https://cdn.tiny.cloud/1/q7648hvurw1lf6ch14apkg45x4hgmmklcvkiytmydm58iqyp/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
     </head>
     <body class="is-preload">
@@ -61,10 +63,10 @@ def render_html_page(post):
 						</div>
 					</div>
 					<div id="links">
-						<a href="index.html">Main</a>
-						<a href="blogindex.html">Blog</a>
-						<a href="resume.html">Resume</a>
-						<a href="index.html#contact">Contact</a>
+						<a href="/index.html">Main</a>
+						<a href="/blogindex.html">Blog</a>
+						<a href="/resume.html">Resume</a>
+						<a href="/index.html#contact">Contact</a>
 						<a id="loginButton" class="link-class" href="javascript:void(0);" onclick="login()">Login</a>
 					</div>
 				</nav>
@@ -73,7 +75,7 @@ def render_html_page(post):
 
         <header id="header">
             <div class="inner">
-                <a href="https://linkedin.com/in/jonathandpolansky" class="image avatar" target="_blank"><img src="images/avatar.jpg" alt="" /></a>
+                <a href="https://linkedin.com/in/jonathandpolansky" class="image avatar" target="_blank"><img src="/images/avatar.jpg" alt="" /></a>
                 <h1>I am <strong>Jonathan Polansky</strong>. <br />
                 Welcome to my blog!</h1>
             </div>
@@ -115,18 +117,18 @@ def render_html_page(post):
             </div>
         </footer>
 
-        <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/js/jquery.poptrox.min.js"></script>
-        <script src="assets/js/browser.min.js"></script>
-        <script src="assets/js/breakpoints.min.js"></script>
-        <script src="assets/js/util.js"></script>
-        <script src="assets/js/menu.js"></script>
-        <script src="assets/js/scroll.js"></script>
-        <script src="assets/js/modal.js"></script>
-        <script src="assets/js/visitorcount.js"></script>
-        <script src="assets/js/auth.js"></script>
-        <script src="assets/js/displayblogpost.js"></script>
-        <script src="assets/js/main.js"></script>
+        <script src="/assets/js/jquery.min.js"></script>
+        <script src="/assets/js/jquery.poptrox.min.js"></script>
+        <script src="/assets/js/browser.min.js"></script>
+        <script src="/assets/js/breakpoints.min.js"></script>
+        <script src="/assets/js/util.js"></script>
+        <script src="/assets/js/menu.js"></script>
+        <script src="/assets/js/scroll.js"></script>
+        <script src="/assets/js/modal.js"></script>
+        <script src="/assets/js/visitorcount.js"></script>
+        <script src="/assets/js/auth.js"></script>
+        <script src="/assets/js/displayblogpost.js"></script>
+        <script src="/assets/js/main.js"></script>
     </body>
 </html>
 """
