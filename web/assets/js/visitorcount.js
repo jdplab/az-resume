414b8f0d98fc0c0a8f4dabc5a9566ce2158f38f7
jplab <jon@jon-polansky.com>
2024-04-02 11:56:02 -0400

    Modify visitor count

--- web/assets/js/visitorcount.js
+++ web/assets/js/visitorcount.js
@@ -26,35 +26,44 @@ const getVisitCount = () => {
             } else {
                 repeatVisit = 1;
             }
+
+            let visitData = sessionStorage.getItem('visitData');
+            if (visitData && repeatVisit) {
+                visitData = JSON.parse(visitData);
+                document.getElementById('unique_visitors').innerText = visitData.unique_visitors;
+                document.getElementById('total_visits').innerText = visitData.total_visits;
+                document.getElementById('visits').innerText = visitData.visits;
+                resolve(visitData);
+            } else {
+                fetchVisitCount(repeatVisit, resolve, reject);
+            }
         } else {
             console.log('Local storage is not supported by this browser');
-            repeatVisit = 0; // Treat every visit as a new visit
+            fetchVisitCount(repeatVisit, resolve, reject);
         }
-
-        // Fetch the visit count from the server
-        fetch(`${functionApi}?repeatVisit=${repeatVisit}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            document.getElementById('unique_visitors').innerText = data.unique_visitors;
-            document.getElementById('total_visits').innerText = data.total_visits;
-            document.getElementById('visits').innerText = data.visits;
-            resolve(data);
-        })
-        .catch(function(error) {
-            console.log('There has been a problem with your fetch operation: ', error);
-            reject(error);
-        });
     });
 };
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    getVisitCount()
-    .catch(error => {
-        console.log('Error getting visit count:', error);
+const fetchVisitCount = (repeatVisit, resolve, reject) => {
+    // Fetch the visit count from the server
+    fetch(`${functionApi}?repeatVisit=${repeatVisit}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (typeof(Storage) !== "undefined") {
+            sessionStorage.setItem('visitData', JSON.stringify(data));
+        }
+        document.getElementById('unique_visitors').innerText = data.unique_visitors;
+        document.getElementById('total_visits').innerText = data.total_visits;
+        document.getElementById('visits').innerText = data.visits;
+        resolve(data);
+    })
+    .catch(function(error) {
+        console.log('There has been a problem with your fetch operation: ', error);
+        reject(error);
     });
-});
+};
