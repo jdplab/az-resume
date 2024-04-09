9d884577dd87e01ad516289a61f3cf107ac1d788
jplab <jon@jon-polansky.com>
2024-04-09 08:06:32 -0400

    Removed some comments. Will comment my code better at another time.

--- function/submitcomment/__init__.py
+++ function/submitcomment/__init__.py
@@ -14,13 +14,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         auth_header = req.headers.get('Authorization')
         if auth_header:
             token = auth_header.split(' ')[1]
-            # Verify the JWT
             try:
                 token_claims = verify_token(token)
             except Exception as e:
                 logging.error(f"Error verifying token: {str(e)}")
                 return func.HttpResponse("Error processing request", status_code=400)
-            # Set token_claims as user info
             firstname = token_claims.get('given_name')
             lastname = token_claims.get('family_name')
             post_id = req.params.get('id')
@@ -36,13 +34,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 cosmos_client = CosmosClient.from_connection_string(os.getenv('resumedb1_DOCUMENTDB'))
                 database = cosmos_client.get_database_client('resumedb')
                 container = database.get_container_client('comments')
-                # Get the current timestamp
                 now_utc = datetime.now(pytz.timezone('UTC'))
                 now_est = now_utc.astimezone(pytz.timezone('US/Eastern'))
                 timestamp = now_est.isoformat()
-                # Create a new comment object
                 new_comment = {'id': str(uuid.uuid4()), 'post_id': post_id, 'comment': comment, 'timestamp': timestamp, 'firstname': firstname, 'lastname': lastname}
-                # Save the comment to CosmosDB
                 container.upsert_item(new_comment)
             except cosmos_exceptions.CosmosResourceNotFoundError:
                 logging.error("Post not found")
